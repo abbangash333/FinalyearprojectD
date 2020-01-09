2181fb2310c99af332814a5f3e988e57e6f4de9e
Aurang Zeb Khan <abbangash333@gmail.com>
2020-01-10 00:22:32 +0500

    Initial commit

---
+++ app/src/main/java/com/example/finalyearprojectd/NotificationsFromUsers.java
@@ -0,0 +1,14 @@
+package com.example.finalyearprojectd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotificationsFromUsers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications_from_users);
+    }
+}
